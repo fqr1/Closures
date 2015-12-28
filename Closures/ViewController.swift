6e748a6b743f384ba452b16010e9db9dba0e3bb2
Fabián Quintero <fabian.quintero@triolabs.mx>
2015-12-28 09:54:51 -0600

    Initial Commit

---
+++ Closures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Fabián on 28/12/15.
+//  Copyright © 2015 Triolabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
